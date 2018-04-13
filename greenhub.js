5062f855d49343e67dd9842f66e14bf196f94cc4
Mikhail Usov <musovx@gmail.com>
2018-04-12 21:19:03 -0700

    first release

--- greenhub.js
+++ greenhub.js
@@ -23,6 +23,7 @@ function initContributionsSvg() {
       rect.setAttribute("width", "10");
       rect.setAttribute("height", "10");
       rect.setAttribute("fill", "#ebedf0");
+      rect.setAttribute("commits", "0");
       rect.setAttribute("x", rx);
       rect.setAttribute("y", ry);
       rect.setAttribute("class", "cRect");
@@ -43,6 +44,7 @@ function initContributionsSvg() {
     element.addEventListener("mouseover", markContribution);
   });
   document.getElementById("clear").addEventListener("click", initContributionsSvg);
+  document.getElementById("downloadButton").addEventListener("click", generateFile);
 }
 
 function markContribution(e) {
@@ -57,15 +59,66 @@ function markContribution(e) {
   switch (currentColor) {
       case "#ebedf0":
           rect.setAttribute("fill", "#c6e48b");
+          rect.setAttribute("commits", "1");
           break;
       case "#c6e48b":
           rect.setAttribute("fill", "#7bc96f");
+          rect.setAttribute("commits", "3");
           break;
       case "#7bc96f":
           rect.setAttribute("fill", "#239a3b");
+          rect.setAttribute("commits", "6");
           break;
       case "#239a3b":
           rect.setAttribute("fill", "#196127");
+          rect.setAttribute("commits", "9");
           break;
   }
+}
+
+function generateFile() {
+  let commits = "";
+  let contributions = document.getElementsByClassName("cRect");
+  let date = new Date();
+  date.setDate(date.getDate() - 366);
+  while (date.getDay() != 6) {
+    date.setDate(date.getDate() - 1);
+  }
+
+  for (let i = 0; i < contributions.length; i++) {
+    // 2017-03-18T12:00:00
+    date.setDate(date.getDate() + 1);
+    let commitsCount = parseInt(contributions[i].getAttribute("commits"));
+    if (commitsCount == 0) continue;
+    let dateString = date.getFullYear() + "-" + ("0" + (date.getMonth()+1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2);
+    dateString += "T12:01:01";
+    for (let j = 0; j < commitsCount; j++) {
+        commits += "GIT_AUTHOR_DATE="+dateString+" GIT_COMMITTER_DATE="+dateString+" git commit --allow-empty -m \"Greenhubing...\" > /dev/null\n"
+    }
+  }
+
+  let user = document.getElementById("user").value;
+  let repo = document.getElementById("repo").value;
+
+  // console.log(fileTemplate(user, repo, commits));
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(fileTemplate(user, repo, commits)));
+  element.setAttribute('download', "greenhub.sh");
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+function fileTemplate(user, repo, commits) {
+  let content = "#!/bin/bash\n";
+  content += "git init "+repo+"\n";
+  content += "cd "+repo+"\n";
+  content += "touch README.md\n";
+  content += "git add README.md\n";
+  content += commits;
+  content += "git remote add origin https://github.com/"+user+"/"+repo+".git\n";
+  content += "git pull\n"
+  content += "git push -u origin master";
+  return content;
 }
